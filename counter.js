93b4e2638c0baf3f70db4738035259c9cbf33314
larry <larryycel@gmail.com>
2024-09-18 04:37:35 -0500

    add: loader img card

--- counter.js
+++ counter.js
@@ -10,58 +10,68 @@ export function setupCard (element) {
   const ts = new Date().getDate()
   const hash = md5(ts + privateKey + publicKey).toString()
 
-  const loader = document.createElement('div')
-  loader.innerHTML = `
+  const cargarLoader = () => {
+    imgC.innerHTML = ''
+    const loader = document.createElement('div')
+    loader.innerHTML = `
 
   <p class="parrafo-c">cargando elemento<span class="punto-c">.</span><span class="punto-c">.</span><span class="punto-c">.</span></p>
 
   `
-  imgC.innerHTML = ''
-  imgC.append(loader)
-
-  // eslint-disable-next-line no-undef
-  const cliente = new XMLHttpRequest()
-  const apiUrl = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+    imgC.append(loader)
+  }
+  cargarLoader()
 
   // actualizo el dom
   function updateDom (index, data) {
-    nombreP.innerText = data.name || 'nombre no disponible'
-    pP.innerText = data.description || 'descripcion no disponible'
-    fechaP.innerText = data.modified ? data.modified.slice(0, 10) : 'fecha no disponible'
-
+    imgC.innerHTML = ''
+    const nombre = data.name || 'nombre no disponible'
+    const description = data.description || 'descripcion no disponible'
+    const fecha = data.modified ? data.modified.slice(0, 10) : 'fecha no disponible'
     const imgPath = `${data.thumbnail.path}.${data.thumbnail.extension}`
 
     const img = document.createElement('img')
     img.src = imgPath || ''
     img.height = 130
     img.width = 130
-    imgC.innerHTML = ''
+
+    nombreP.innerText = nombre
+    pP.innerText = description
+    fechaP.innerText = fecha
     imgC.appendChild(img)
   }
 
-  cliente.addEventListener('readystatechange', function () {
+  function fetcherData () {
     // eslint-disable-next-line no-undef
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      const res = JSON.parse(this.responseText)
-      const data = res.data.results
-      if (data.length > 0) {
-        let currentIndex = 0
-        // primera muestra
-        updateDom(currentIndex, data[currentIndex])
-        // cambiar caundo hacen click
-        document.getElementById('btn-active').addEventListener('click', function () {
-          currentIndex += 1 % data.length
-          updateDom(currentIndex, data[currentIndex])
-        })
-      } else {
-        console.log('no ahi resultados')
-      }
-      // eslint-disable-next-line no-undef
-    } else if (this.readyState === XMLHttpRequest.DONE) {
-      console.log('no se pudo realizar la peticion', this.status)
-    }
-  })
+    const cliente = new XMLHttpRequest()
+    const apiUrl = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
 
-  cliente.open('GET', apiUrl)
-  cliente.send()
+    cliente.addEventListener('readystatechange', function () {
+    // eslint-disable-next-line no-undef
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const res = JSON.parse(this.responseText)
+        const data = res.data.results
+        if (data.length > 0) {
+          let currentIndex = 0
+          // primera muestra
+          updateDom(currentIndex, data[currentIndex])
+          // cambiar caundo hacen click
+          document.getElementById('btn-active').addEventListener('click', function () {
+            currentIndex = (currentIndex + 1) % data.length
+            cargarLoader()
+            setTimeout(() => { updateDom(currentIndex, data[currentIndex]) }, 150)
+          })
+        } else {
+          console.log('no ahi resultados')
+        }
+      // eslint-disable-next-line no-undef
+      } else if (this.readyState === XMLHttpRequest.DONE) {
+        console.log('no se pudo realizar la peticion', this.status)
+      }
+    })
+
+    cliente.open('GET', apiUrl)
+    cliente.send()
+  }
+  fetcherData()
 }
